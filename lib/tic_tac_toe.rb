c5cc18c9f1a9ec93018830b00366c6e91239d18d
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-03-05 04:34:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -159,4 +159,15 @@ def winner(board)
 end
 
 def play(board)
+  '''
+  until the game is over
+    take turns
+  end
+  
+  if the game was won
+    congratulate the winner
+  else if the game was a draw
+    tell the players it has been a draw
+  end
+  '''
 end
