dfc57235995c62653217d875362e5b89ad9f3cbb
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-03-05 04:19:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -113,15 +113,11 @@ def won?(board)
     # checking for x
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination # return the win_combination indexes that won.
-    else
-      false
     end
 
     # checking for o
     if position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination # return the win_combination indexes that won.
-    else
-      false
     end
   end
 
@@ -136,4 +132,7 @@ def full?(board)
   end
 
   return true
+end
+
+def draw?(board)
 end
